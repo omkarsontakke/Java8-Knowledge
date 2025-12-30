544da47a487979960b15521288942fb051b2cb43
Omkar Sontakke <omkarsontakke66527@gmail.com>
2025-12-30 15:12:50 +0530

    Added Feature B

--- src/main/java/org/example/MockTest/Mock_30122025.java
+++ src/main/java/org/example/MockTest/Mock_30122025.java
@@ -4,12 +4,14 @@ public class Mock_30122025 {
     public static void main(String[] args) {
         System.out.println("Started day 30 of java 8");
     }
-
     public void newFeature(){
         System.out.println("This is my new feature");
     }
     public void newFeatureA(){
         System.out.println("This is my new feature A");
+    }
+    public void newFeatureB(){
+        System.out.println("This is my new feature B");
     }
 }
 
