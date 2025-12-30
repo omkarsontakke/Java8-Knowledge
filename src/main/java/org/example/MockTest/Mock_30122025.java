5650bf041cd7c6ef8e0c3dcc611ea78eea41d0da
Omkar Sontakke <omkarsontakke66527@gmail.com>
2025-12-30 14:42:56 +0530

    Add new feat in day 30 by omkar

--- src/main/java/org/example/MockTest/Mock_30122025.java
+++ src/main/java/org/example/MockTest/Mock_30122025.java
@@ -4,4 +4,10 @@ public class Mock_30122025 {
     public static void main(String[] args) {
         System.out.println("Started day 30 of java 8");
     }
+
+    public void newFeature(){
+        System.out.println("This is my new feature");
+    }
 }
+
+
