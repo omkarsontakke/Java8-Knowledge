a85e74cfb91594e814c49449017b13506d147250
Omkar Sontakke <95247012+omkarsontakke@users.noreply.github.com>
2026-01-01 13:13:13 +0530

    Add the another approach for Q.4

--- src/main/java/org/example/MockTest/Mock_01012026.java
+++ src/main/java/org/example/MockTest/Mock_01012026.java
@@ -49,7 +49,7 @@ public class Mock_01012026 {
         Set<Integer> uniqueEle = new HashSet<>();
 
         List<Integer> duplicateElementInList = numbers1.stream().filter(n1 -> !uniqueEle.add(n1)).sorted().toList();
-
+        // List<Integer> duplicateElementInList = numbers1.stream().filter(n1 -> String.valueOf(n1).startWith("1").toList();
         System.out.println(duplicateElementInList);
 
     }
