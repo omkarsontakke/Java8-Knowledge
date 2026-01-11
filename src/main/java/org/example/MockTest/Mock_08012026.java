3ad9294a680b33f00b28020b2cecdc7176cdba29
Omkar Anita Anil Sontakke <omkarsontakke66527@gmail.com>
2026-01-11 14:02:05 +0530

    Adding file of 8 Jan

---
+++ src/main/java/org/example/MockTest/Mock_08012026.java
@@ -0,0 +1,7 @@
+package org.example.MockTest;
+
+public class Mock_08012026 {
+    public static void main(String[] args) {
+        System.out.println("Complete the pending 08 Jan task...");
+    }
+}
